161542c3688eb16e788b566cce09f744a4effc6e
vivian <rolleguido@gmail.com>
2023-05-24 14:00:22 +0200

    Fix for board

--- programs/NBODY/PROFILING/build.sh
+++ programs/NBODY/PROFILING/build.sh
@@ -6,12 +6,16 @@ cd "${SCRIPT_DIR}"
 
 rm -rf build
 mkdir build
-cmake \
+export CXX=/usr/bin/g++-8
+(
+    cd build && \
+    cmake \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DBOOST_ROOT="~/Documents/Thesis/margot/boost_1_60_0/install" \
-    -Declipse-paho-mqtt-c_DIR="~/Documents/Thesis/margot/paho.mqtt.c/install/lib/cmake/eclipse-paho-mqtt-c/" \
-    -Dmargot_heel_generator_DIR="~/Documents/Thesis/margot/core/install/lib/cmake/margot" \
-    -S . -B build
-cmake --build build
+    -DBOOST_ROOT="~/Vivian/Thesis/margot/boost_1_60_0/install" \
+    -Declipse-paho-mqtt-c_DIR="~/Vivian/Thesis/margot/paho.mqtt.c/install/lib/cmake/eclipse-paho-mqtt-c/" \
+    -Dmargot_heel_generator_DIR="~/Vivian/Thesis/margot/core/install/lib/cmake/margot" \
+    .. && \
+    cmake --build . \
+)
 
 cd "${CWD_DIR}"
--- programs/NBODY/common/libs/Nbody/src/NbodyCpu.cc
+++ programs/NBODY/common/libs/Nbody/src/NbodyCpu.cc
@@ -9,16 +9,16 @@ namespace NbodyCpu
     void NbodyCpu::run()
     {
         #pragma omp parallel for num_threads(nThreads)
-        for (::Nbody::Body& current : bodies){
+        for (auto current = bodies.begin(); current < bodies.end(); current++){
 
             float Fx = 0.0f; 
             float Fy = 0.0f; 
             float Fz = 0.0f;
 
-            for (const ::Nbody::Body& other : bodies) {
-                const float dx = other.pos.x - current.pos.x;
-                const float dy = other.pos.y - current.pos.y;
-                const float dz = other.pos.z - current.pos.z;
+            for (auto other = bodies.begin(); other < bodies.end(); other++) {
+                const float dx = other->pos.x - current->pos.x;
+                const float dy = other->pos.y - current->pos.y;
+                const float dz = other->pos.z - current->pos.z;
 
                 const float distSqr = dx*dx + dy*dy + dz*dz + NBODY_SOFTENING;
                 const float invDist = 1/sqrtf(distSqr);
@@ -29,17 +29,18 @@ namespace NbodyCpu
                 Fz += dz * invDist3;
             }
 
-            current.vel.x += dt*Fx; 
-            current.vel.y += dt*Fy; 
-            current.vel.z += dt*Fz;
+            current->vel.x += dt*Fx; 
+            current->vel.y += dt*Fy; 
+            current->vel.z += dt*Fz;
         }
 
-        #pragma omp parallel for num_threads(nThreads)
-        for (::Nbody::Body& body : bodies){
-            body.pos.x += body.vel.x*dt; 
-            body.pos.y += body.vel.y*dt; 
-            body.pos.z += body.vel.z*dt;
+        #pragma parallel omp for num_threads(nThreads)
+        for (auto body = bodies.begin(); body < bodies.end(); body++){
+            body->pos.x += body->vel.x*dt; 
+            body->pos.y += body->vel.y*dt; 
+            body->pos.z += body->vel.z*dt;
         }
+
     }
 
     std::vector<::Nbody::Body> NbodyCpu::getResult() { return bodies; };
