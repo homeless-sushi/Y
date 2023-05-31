19bce1b4487432e2bd6c4b135c294a5983c37d4e
Guido Rolle <rolleguido@gmail.com>
2023-05-31 13:45:02 +0200

    Add timers to NBODY benchmark

--- programs/NBODY/BENCHMARK/NBODY/src/nbody.cc
+++ programs/NBODY/BENCHMARK/NBODY/src/nbody.cc
@@ -19,6 +19,7 @@
 #include "AppRegisterCommon/Semaphore.h"
 
 #include "AppRegisterClient/AppRegister.h"
+#include "AppRegisterClient/Utils.h"
 
 #include <margot/margot.hpp>
 
@@ -35,6 +36,10 @@ bool stop = false;
 
 int main(int argc, char *argv[])
 {
+    std::cout << "EVENT,TYPE,DEVICE,TIMESTAMP" << std::endl;
+ 
+    //START: SETUP
+    std::cout << "SETUP,START,CPU," << now() << std::endl;
     po::options_description desc(SetupOptions());
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -59,6 +64,7 @@ int main(int argc, char *argv[])
 
     unsigned int deviceId = 0;
     unsigned int gpuBlockSizeExp = 0;
+
     Knobs::DEVICE device;
     Knobs::GpuKnobs::BLOCK_SIZE gpuBlockSize;
 
@@ -69,25 +75,37 @@ int main(int argc, char *argv[])
         gpuBlockSizeExp,
         gpuBlockSize
     );
+    std::cout << "SETUP,STOP,CPU," << now() << std::endl;
+    //STOP: SETUP
 
     //Spinlock
+    //START: WAIT REGISTRATION
+    std::cout << "WAIT REGISTRATION,START,CPU," << now() << std::endl;
     while(true){
         if(isRegistered(data)){
             setTickStartTime(data);
             break;
         }
     }
+    std::cout << "WAIT REGISTRATION,STOP,CPU," << now() << std::endl;
+    //STOP: WAIT REGISTRATION
 
     bool error = false;
     while(!stop && !error){
 
         //Read knobs
+        //START: CONTROLLER PULL
+        std::cout << "CONTROLLER PULL,START,CPU," << now() << std::endl;
         error = binarySemaphoreWait(dataSemId);
         cpuThreads = getNCpuCores(data);
         device = getUseGpu(data) ? Knobs::DEVICE::GPU : Knobs::DEVICE::CPU;
         error = binarySemaphorePost(dataSemId);
         deviceId = static_cast<unsigned int>(device);
+        std::cout << "CONTROLLER PULL,STOP,CPU," << now() << std::endl;
+        //STOP: CONTROLLER PULL
 
+        //START: MARGOT PULL
+        std::cout << "MARGOT PULL,START,CPU," << now() << std::endl;
         if(margot::nbody::update(cpuThreads, deviceId, gpuBlockSizeExp, precision)){
             CastKnobs(
                 gpuBlockSizeExp,
@@ -95,9 +113,12 @@ int main(int argc, char *argv[])
             );
             margot::nbody::context().manager.configuration_applied();
         }
+        std::cout << "MARGOT PULL,STOP,CPU," << now() << std::endl;
+        //STOP: MARGOT PULL
 
+        //START: WIND UP
+        std::cout << "WIND UP,START,CPU," << now() << std::endl;
         margot::nbody::start_monitors();
-
         std::string inputFileURL(vm["input-file"].as<std::string>());
         std::vector<Nbody::Body> bodies;
         float targetSimulationTime;
@@ -116,12 +137,21 @@ int main(int argc, char *argv[])
             static_cast<Nbody::Nbody*>(new NbodyCuda::NbodyCuda(bodies, actualTimeStep, gpuBlockSize)) :
             static_cast<Nbody::Nbody*>(new NbodyCpu::NbodyCpu(bodies, actualTimeStep, cpuThreads))
         );
+        std::cout << "WIND UP,STOP,CPU," << now() << std::endl;
+        //STOP: WIND UP
+
+        //START: KERNEL
+        std::cout << "KERNEL,START," << Knobs::DeviceToString(device) << "," << now() << std::endl;
         float actualSimulationTime;
         for(actualSimulationTime = 0.f; actualSimulationTime < targetSimulationTime; actualSimulationTime+=actualTimeStep){
             nbody->run();
         }
         bodies = nbody->getResult();
+        std::cout << "KERNEL,STOP," << Knobs::DeviceToString(device) << "," << now() << std::endl;
+        //STOP: KERNEL
         
+        //START: WIND DOWN
+        std::cout << "WIND DOWN,START,CPU," << now() << std::endl;
         if(vm.count("output-file")){
             Nbody::WriteBodyFile(vm["output-file"].as<std::string>(), 
                 bodies,
@@ -129,16 +159,25 @@ int main(int argc, char *argv[])
                 actualTimeStep
             );
         }
+        std::cout << "WIND DOWN,STOP,CPU," << now() << std::endl;
+        //START: WIND DOWN
 
+        //START: MARGOT PUSH
+        std::cout << "MARGOT PUSH,START,CPU," << now() << std::endl;
         margot::nbody::stop_monitors();
         margot::nbody::push_custom_monitor_values();
-        margot::nbody::log();
+        std::cout << "MARGOT PUSH,STOP,CPU," << now() << std::endl;
+        //STOP: MARGOT PUSH
 
         //Add tick
+        //START: CONTROLLER PUSH
+        std::cout << "CONTROLLER PUSH,START,CPU," << now() << std::endl;
         autosleep(data, targetThroughput);
         error = binarySemaphoreWait(dataSemId);
         addTick(data, 1);
         error = binarySemaphorePost(dataSemId);
+        std::cout << "CONTROLLER PUSH,STOP,CPU," << now() << std::endl;
+        //STOP: CONTROLLER PUSH
     }
 
     registerDetach(data);
