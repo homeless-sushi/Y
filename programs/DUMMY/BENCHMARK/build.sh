d478f4774177f07321e63918e8fb0aac0a665891
Guido Rolle <rolleguido@gmail.com>
2023-06-16 15:11:28 +0200

    Fix for board

--- programs/DUMMY/BENCHMARK/build.sh
+++ programs/DUMMY/BENCHMARK/build.sh
@@ -6,11 +6,14 @@ cd "${SCRIPT_DIR}"
 
 rm -rf build
 mkdir build
-cmake \
+export CXX=/usr/bin/g++-8
+(
+    cd build && \
+    cmake \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DBOOST_ROOT="~/Documents/Thesis/margot/boost_1_60_0/install" \
-    -S . -B build
-cmake --build build
+    -DBOOST_ROOT="~/Vivian/Thesis/margot/boost_1_60_0/install" \
+    .. && \
+    cmake --build . \
+)
 
 cd "${CWD_DIR}"
-
