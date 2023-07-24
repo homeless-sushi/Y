9dda5609467cc07612826c1c9411f0b16108b9a9
Guido Rolle <rolleguido@gmail.com>
2023-07-24 20:56:10 +0200

    Fix CUTCP data_gen and input data

--- programs/CUTCP/common/scripts/data_gen/data_gen.py
+++ programs/CUTCP/common/scripts/data_gen/data_gen.py
@@ -1,9 +1,17 @@
 import argparse
 import os
+import math
 import sys
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 REFERENCE_URL = os.path.join(SCRIPT_DIR,"reference","reference.txt")
+
+class Atom:
+    def __init__(self, x, y, z, q):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.q = q
 
 def setup_args() :
 
@@ -17,10 +25,21 @@ def setup_args() :
     )
 
     parser.add_argument(
-        '--size',
-        type=int,
-        required=True,
-        help='size of the output'
+        'X',
+        type=float,
+        help='len of lattice in X'
+    )
+
+    parser.add_argument(
+        'Y',
+        type=float,
+        help='len of lattice in Y'
+    )
+
+    parser.add_argument(
+        'Z',
+        type=float,
+        help='len of lattice in Z'
     )
 
     parser.add_argument(
@@ -37,7 +56,9 @@ def main():
     args = parser.parse_args()
 
     reference_url = REFERENCE_URL
-    n = args.size
+    xSize = args.X
+    ySize = args.Y
+    zSize = args.Z
     output_url = args.output_file
 
     # The reference is the CUTCP input data included in 
@@ -48,12 +69,31 @@ def main():
     # was an assumption in the paper, we can simply truncate
     # the file.
 
+    atoms = []
     with open(reference_url, "r") as reference_file :
-        data_lines = list(reference_file)[0:n]
+        
+        for data_line in reference_file:
+            x, y, z, q = map(float, data_line.strip().split())
+            atoms.append(Atom(x, y, z, q))
 
-        with open(output_url, "w") as output_file :
-            for data_line in data_lines :
-                output_file.write(data_line)
+        xMin, yMin, zMin = math.inf, math.inf, math.inf
+
+        for atom in atoms:
+            xMin = min(xMin, atom.x)
+            yMin = min(yMin, atom.y)
+            zMin = min(zMin, atom.z)
+
+        xLim = xMin + xSize
+        yLim = yMin + ySize
+        zLim = zMin + zSize
+        atoms = [ atom for atom in atoms if
+                    atom.x <= xLim and
+                    atom.y <= yLim and
+                    atom.z <= zLim ]
+        
+    with open(output_url, "w") as output_file :
+        for atom in atoms :
+            output_file.write(f"{atom.x:.3f} {atom.y:.3f} {atom.z:.3f} {atom.q:.3f}\n")
 
     return 0
 
