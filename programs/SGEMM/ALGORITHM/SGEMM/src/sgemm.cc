c0bfd2ef1e61ed57d3599de12ae5d3da023766a1
Guido Rolle <rolleguido@gmail.com>
2023-10-16 15:13:33 +0200

    Add knobs to SGEMM ALGORITHM

--- programs/SGEMM/ALGORITHM/SGEMM/src/sgemm.cc
+++ programs/SGEMM/ALGORITHM/SGEMM/src/sgemm.cc
@@ -8,11 +8,19 @@
 #include "Sgemm/SgemmCuda.h"
 #include "Sgemm/ReadWrite.h"
 
+#include <Knobs/Device.h>
+
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
 
 po::options_description SetupOptions();
+void CastKnobs(
+    unsigned int cpuTileSizeExp,
+    unsigned int gpuTileSizeExp,
+    unsigned int& cpuTileSize,
+    Knobs::GpuKnobs::TILE_SIZE& gpuTileSize
+);
 
 int main(int argc, char *argv[])
 {
@@ -25,6 +33,22 @@ int main(int argc, char *argv[])
         return 0;
     }
 
+    unsigned int cpuTileSizeExp = vm["cpu-tile-exp"].as<unsigned>();
+    unsigned int gpuTileSizeExp = vm["gpu-tile-exp"].as<unsigned>();
+    
+    Knobs::DEVICE device = vm["gpu"].as<bool>() ? Knobs::DEVICE::GPU : Knobs::DEVICE::CPU;
+    unsigned int cpuTileSize;
+    Knobs::GpuKnobs::TILE_SIZE gpuTileSize;
+
+    CastKnobs(
+        cpuTileSizeExp,
+        gpuTileSizeExp,
+        cpuTileSize,
+        gpuTileSize
+    );
+
+    unsigned int cpuThreads = vm["cpu-threads"].as<unsigned>();;
+
     std::string inputAUrl(vm["input-A"].as<std::string>());
     std::string inputBUrl(vm["input-B"].as<std::string>());
     std::string inputCUrl(vm["input-C"].as<std::string>());
@@ -32,16 +56,36 @@ int main(int argc, char *argv[])
     Sgemm::Matrix b(Sgemm::ReadMatrixFile(inputBUrl));
     Sgemm::Matrix c(Sgemm::ReadMatrixFile(inputCUrl));
 
-    //Sgemm::SgemmCpu sgemm(1,1,a,b,c,1,4);
-    Sgemm::SgemmCuda sgemm(1,1,a,b,c,16);
-    sgemm.run();
+    std::unique_ptr<Sgemm::Sgemm> sgemm( 
+        device == Knobs::DEVICE::GPU ?
+        static_cast<Sgemm::Sgemm*>(new Sgemm::SgemmCuda(1,1,a,b,c,gpuTileSize)) :
+        static_cast<Sgemm::Sgemm*>(new Sgemm::SgemmCpu(1,1,a,b,c,cpuThreads,cpuTileSize))
+    );
+
+    unsigned times = vm["times"].as<unsigned>();
+    for(unsigned i = 0; i < times; i++){
+        sgemm->run();
+    }
     
-    Sgemm::Matrix res(sgemm.getResult());
+    Sgemm::Matrix res(sgemm->getResult());
     if(vm.count("output-file")){
         Sgemm::WriteMatrixFile(vm["output-file"].as<std::string>(), res);
     }
     
     return 0;
+}
+
+void CastKnobs(
+    unsigned int cpuTileSizeExp,
+    unsigned int gpuTileSizeExp,
+    unsigned int& cpuTileSize,
+    Knobs::GpuKnobs::TILE_SIZE& gpuTileSize
+)
+{
+    cpuTileSize = 16 << cpuTileSizeExp;
+    gpuTileSize = static_cast<Knobs::GpuKnobs::TILE_SIZE>(
+        Knobs::GpuKnobs::TILE_8 << gpuTileSizeExp
+    );
 }
 
 po::options_description SetupOptions()
@@ -53,6 +97,11 @@ po::options_description SetupOptions()
     ("input-B,B", po::value<std::string>(), "input file with matrix B")
     ("input-C,C", po::value<std::string>(), "input file with matrix C")
     ("output-file,O", po::value<std::string>(), "ouput file with result matrix")
+    ("gpu", po::bool_switch(), "use gpu")
+    ("cpu-threads", po::value<unsigned int>()->default_value(1), "number of cpu threads")
+    ("cpu-tile-exp", po::value<unsigned int>()->default_value(0), "tile exp; block size = 16*2^X")
+    ("gpu-tile-exp", po::value<unsigned int>()->default_value(0), "tile exp; block size = 8*2^X")
+    ("times,T", po::value<unsigned int>()->default_value(1), "repeat the kernel T times")
     ;
 
     return desc;
